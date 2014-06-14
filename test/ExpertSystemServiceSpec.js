cba0a8f8f0ded41e1595a92add5fbc1bb36c34dd
jmgolfier <jmgolfier@gmail.com>
2014-06-14 22:36:38 +0200

    test expertsystemservice

--- test/ExpertSystemServiceSpec.js
+++ test/ExpertSystemServiceSpec.js
@@ -1,16 +1,23 @@
 describe("ExpertSystemService", function () {
     var expertService;
-    beforeEach(function () {
 
-    });
+    beforeEach(module(APP_NAME));
+
+    beforeEach(inject(function (ExpertSystemService) {
+        expertService = new ExpertSystemService();
+    }));
 
     describe("initialization", function () {
         it("should be defined", function () {
-           expect(expertService).toBeDefined();
+            module(function ($provide) {
+                $provide.value("KnowledgeRepository", "test");
+            });
+
+            expect(expertService).toBeDefined();
         });
 
         it("should have facts array empty",function(){
-            expect(expertService)
+//            expect(expertService)
         });
     });
 });
