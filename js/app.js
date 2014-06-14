9d5850fc0710f3292c3526a9f46c2172b062c0f2
epayet <jakspinning@gmail.com>
2014-06-15 00:16:25 +0200

    Started Knowledge view

--- js/app.js
+++ js/app.js
@@ -24,6 +24,7 @@ angular.module(APP_NAME, [
 
     config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/battle', {templateUrl: 'views/battle.html', controller: 'BattleController'});
+        $routeProvider.when('/knowledge', {templateUrl: 'views/fillKnowledge.html', controller: 'FillKnowledgeController'});
         $routeProvider.otherwise({redirectTo: '/battle'});
     }]).
 
